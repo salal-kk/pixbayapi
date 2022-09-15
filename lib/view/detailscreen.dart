2bd6e10fb5854b9752b93a2e90490fe2801aa16a
salal-kk <kkmohammedsalal@gmail.com>
2022-09-14 23:21:29 -0700

    first commit

--- lib/view/detailscreen.dart
+++ lib/view/detailscreen.dart
@@ -5,25 +5,28 @@ import 'package:machine_test_pixbay_api/control/apicall.dart';
 import 'package:machine_test_pixbay_api/model/modelclass.dart';
 
 class Detailsscreen extends StatefulWidget {
-  Detailsscreen({Key? key, required this.index});
-  int index=0;
+  Detailsscreen({Key? key, required this.index, this.keyvalue});
+  int? index = 0;
+  String? keyvalue = "";
   @override
   State<Detailsscreen> createState() => _DetailsscreenState();
 }
 
 class _DetailsscreenState extends State<Detailsscreen> {
+  // late int indexofdata = 2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: FutureBuilder<Pixbay>(
-            future: getPictureFromApi(),
+            future: getPictureFromApi(searchkeyword: widget.keyvalue),
             builder: ((context, snapshot) {
               if (snapshot.hasData) {
                 return Center(
                   child: Container(
                     child: Image.network(
-                        snapshot.data?.hits![widget.index].largeImageURL ?? ""),
+                        snapshot.data?.hits![ widget.index??0].largeImageURL ??
+                            ""),
                   ),
                 );
               } else {
--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -15,6 +15,7 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   late FocusNode myFocusNode;
   late ScrollController controller;
+  int indeexof = 0;
 
   final editingcontroller = TextEditingController();
 
@@ -24,7 +25,7 @@ class _HomepageState extends State<Homepage> {
     myFocusNode = FocusNode();
     controller = ScrollController();
     editingcontroller.text = "";
-    getPictureFromApi(searchkeyword: searchkeyWord);
+    //getPictureFromApi(searchkeyword: searchkeyWord);
     super.initState();
   }
 
@@ -71,118 +72,142 @@ class _HomepageState extends State<Homepage> {
                     },
                     icon: const Icon(Icons.search_outlined),
                     label: const Text("Search Here")),
-                FutureBuilder<Pixbay>(
-                  future: getPictureFromApi(searchkeyword: searchkeyWord),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData && editingcontroller.text != "") {
-                      return ListView.builder(
-                        controller: controller,
-                        shrinkWrap: true,
-                        itemCount: snapshot.data!.hits?.length,
-                        itemBuilder: ((context, index) {
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          Detailsscreen(index: index)));
-                            },
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15)),
-                              color: Colors.black,
-                              child: SizedBox(
-                                width: double.infinity,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    SizedBox(
+                searchkeyWord != null
+                    ? FutureBuilder<Pixbay>(
+                        future: getPictureFromApi(searchkeyword: searchkeyWord),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData &&
+                              editingcontroller.text != "") {
+                            return ListView.builder(
+                              controller: controller,
+                              shrinkWrap: true,
+                              itemCount: snapshot.data!.hits?.length,
+                              itemBuilder: ((context, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      for (int i = 0;
+                                          i < snapshot.data!.hits!.length;
+                                          i++) {
+                                        if (snapshot.data!.hits![i].id ==
+                                            index) {
+                                          indeexof = i;
+
+                                          break;
+                                        }
+                                      }
+                                      print(index);
+                                      return Detailsscreen(
+                                          keyvalue: editingcontroller.text,
+                                          index: index ?? 0);
+                                    }));
+                                  },
+                                  child: Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    color: Colors.black,
+                                    child: SizedBox(
                                       width: double.infinity,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Card(
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(15)),
-                                          child: Image.network(
-                                            snapshot.data?.hits![index]
-                                                    .previewURL ??
-                                                "Loading....",
-                                            fit: BoxFit.fitWidth,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          SizedBox(
+                                            width: double.infinity,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Card(
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            15)),
+                                                child: Image.network(
+                                                  snapshot.data?.hits![index]
+                                                          .previewURL ??
+                                                      "Loading....",
+                                                  fit: BoxFit.fitWidth,
+                                                ),
+                                              ),
+                                            ),
                                           ),
-                                        ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: SizedBox(
+                                              width: double.infinity,
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                children: <Widget>[
+                                                  //   likes
+                                                  const Icon(
+                                                    Icons.favorite_border,
+                                                    color: Colors.white,
+                                                  ),
+                                                  Text(
+                                                    snapshot.data?.hits![index]
+                                                            .likes
+                                                            .toString() ??
+                                                        "",
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  // favorites
+                                                  const Padding(
+                                                    padding: EdgeInsets.only(
+                                                        left: 8.0),
+                                                    // ignore: unnecessary_const
+                                                    child: const Icon(
+                                                        Icons.download_done,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    snapshot.data?.hits![index]
+                                                            .downloads
+                                                            .toString() ??
+                                                        "",
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  // comments
+                                                  const Padding(
+                                                    padding: EdgeInsets.only(
+                                                        left: 8.0),
+                                                    child: Icon(
+                                                        Icons
+                                                            .chat_bubble_outline,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    snapshot.data?.hits![index]
+                                                            .comments
+                                                            .toString() ??
+                                                        "",
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: SizedBox(
-                                        width: double.infinity,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: <Widget>[
-                                            //   likes
-                                            const Icon(
-                                              Icons.favorite_border,
-                                              color: Colors.white,
-                                            ),
-                                            Text(
-                                              snapshot.data?.hits![index].likes
-                                                      .toString() ??
-                                                  "",
-                                              style: const TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                            // favorites
-                                            const Padding(
-                                              padding:
-                                                  EdgeInsets.only(left: 8.0),
-                                              // ignore: unnecessary_const
-                                              child: const Icon(
-                                                  Icons.download_done,
-                                                  color: Colors.white),
-                                            ),
-                                            Text(
-                                              snapshot.data?.hits![index]
-                                                      .downloads
-                                                      .toString() ??
-                                                  "",
-                                              style: const TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                            // comments
-                                            const Padding(
-                                              padding:
-                                                  EdgeInsets.only(left: 8.0),
-                                              child: Icon(
-                                                  Icons.chat_bubble_outline,
-                                                  color: Colors.white),
-                                            ),
-                                            Text(
-                                              snapshot.data?.hits![index]
-                                                      .comments
-                                                      .toString() ??
-                                                  "",
-                                              style: const TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        }),
-                      );
-                    } else {
-                      return const Text("Please Enter Any Keyword To Search");
-                    }
-                  },
-                )
+                                  ),
+                                );
+                              }),
+                            );
+                          } else {
+                            return const Text(
+                                "Please Enter Any Keyword To Search");
+                          }
+                        },
+                      )
+                    : Center(
+                        child: Text("Search something"),
+                      )
               ],
             ),
           ),
